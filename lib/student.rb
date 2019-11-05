b3b9d2756dd0da7746ed92f82b480873ffa902c9
Buford Eeds <bufordeeds8@gmail.com>
2019-11-05 15:34:18 -0600

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
+
+    attr_reader :knowledge
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(learning)
+        @knowledge << learning
+    end
 
 end
+
